84ae9fc7efc838ef04f5d61869be2256624493f7
sagar1024 <141512763+sagar1024@users.noreply.github.com>
2023-10-10 20:59:37 +0530

    Add files via upload

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../Css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+            </div>
+            <div className="copyright">
+                <p className="copyright-blog">Created By:</p>
+                <p className="copyright-blog">Shubham Mishra</p>
+                <p className="copyright-blog">Sagar</p>
+                <p className="copyright-blog">Kaushik</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
